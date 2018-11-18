387ee53d68acd5f19178b93d6319f7e78f7a2eb1
Kevin Greenman <kgreenman77@gmail.com>
2018-11-18 11:39:59 -0500

    Added sample outputs and three unit tests. All tests passed

--- vasp_postprocess/tests/test_vasp_postprocess.py
+++ vasp_postprocess/tests/test_vasp_postprocess.py
@@ -1,12 +1,56 @@
+#!/usr/bin/env python3
+#  coding=utf-8
+
 """
-Unit and regression test for the vasp_postprocess package.
+Tests for the vasp_postprocess package.
 """
 
 # Import package, test suite, and other packages as needed
-import vasp_postprocess
-import pytest
-import sys
+import os
+import unittest
+import logging
+from lattice_and_bandgap import main
+from common import capture_stdout, capture_stderr, diff_lines, silent_remove
 
-def test_vasp_postprocess_imported():
-    """Sample test, will always pass so long as import statement worked"""
-    assert "vasp_postprocess" in sys.modules
+__author__ = 'kpgreenm'
+
+# logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+DISABLE_REMOVE = logger.isEnabledFor(logging.DEBUG)
+DATA_DIR = os.path.join(os.path.dirname(__file__), './data')
+
+
+# class TestNoOutput(unittest.TestCase):
+    # TODO: Make EIGENVAL and CONTCAR inputs to main
+    # def testMissingFile(self):
+    #     test_input = ["ghost.txt", "-e", ".txt"]
+    #     if logger.isEnabledFor(logging.DEBUG):
+    #         main(test_input)
+    #     with capture_stderr(main, test_input) as output:
+    #         self.assertTrue("No such file or directory" in output)
+
+
+class TestExpectedOutput(unittest.TestCase):
+    # These will show example usage
+    def testStructural(self):
+        try:
+            main(['-s'])
+            self.assertFalse(diff_lines('unitcell.txt', './tests/unitcell_correct.txt'))
+        finally:
+            silent_remove('unitcell.txt')
+
+    def testElectronic(self):
+        try:
+            main(['-e'])
+            self.assertFalse(diff_lines('bandgap.txt', './tests/bandgap_correct.txt'))
+        finally:
+            silent_remove('bandgap.txt')
+
+    def testStructuralAndElectronic(self):
+        try:
+            main(['-a'])
+            self.assertFalse(diff_lines('unitcell.txt', './tests/unitcell_correct.txt'))
+            self.assertFalse(diff_lines('bandgap.txt', './tests/bandgap_correct.txt'))
+        finally:
+            silent_remove('unitcell.txt')
+            silent_remove('bandgap.txt')
