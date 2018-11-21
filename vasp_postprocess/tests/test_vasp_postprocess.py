33dac898e8da727226b762f429a037756c2d92b3
Kevin Greenman <kgreenman77@gmail.com>
2018-11-21 13:05:41 -0500

    Changed map() to list(map()) to make compatible with Python 3.x

--- vasp_postprocess/tests/test_vasp_postprocess.py
+++ vasp_postprocess/tests/test_vasp_postprocess.py
@@ -20,41 +20,23 @@ DISABLE_REMOVE = logger.isEnabledFor(logging.DEBUG)
 DATA_DIR = os.path.join(os.path.dirname(__file__), './data')
 
 
-# class TestNoOutput(unittest.TestCase):
-    # TODO: Change name of EIGENVAL and CONTCAR, test, and turn back
-    # def testMissingFile(self):
-    #     test_input = ["ghost.txt", "-e", ".txt"]
-    #     if logger.isEnabledFor(logging.DEBUG):
-    #         main(test_input)
-    #     with capture_stderr(main, test_input) as output:
-    #         self.assertTrue("No such file or directory" in output)
-
-
 class TestExpectedOutput(unittest.TestCase):
     # These will show example usage
-    def testStructural(self):
+    def testStructuralOutput(self):
         try:
             main(['-s'])
             self.assertFalse(diff_lines('unitcell.txt', './tests/unitcell_correct.txt'))
         finally:
             silent_remove('unitcell.txt')
 
-    def testElectronic224Bands(self):
+    def testElectronicOutput(self):
         try:
             main(['-e'])
             self.assertFalse(diff_lines('bandgap.txt', './tests/bandgap_correct.txt'))
         finally:
             silent_remove('bandgap.txt')
 
-    # def testElectronic320Bands(self):
-    # TODO: Make this work
-    #     try:
-    #         main(['-e'])
-    #         self.assertFalse(diff_lines('bandgap.txt', './tests/bandgap_correct.txt'))
-    #     finally:
-    #         silent_remove('bandgap.txt')
-
-    def testStructuralAndElectronic(self):
+    def testStructuralAndElectronicOutput(self):
         try:
             main(['-a'])
             self.assertFalse(diff_lines('unitcell.txt', './tests/unitcell_correct.txt'))
